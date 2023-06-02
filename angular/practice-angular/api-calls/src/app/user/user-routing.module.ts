a18051f79b319deb79f5654271c8d144527912b9
Patelsweta12 <swetapatel11114@gmail.com>
2023-06-02 09:18:48 +0530

    basic form changes

---
+++ angular/practice-angular/api-calls/src/app/user/user-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user.component';
+
+const routes: Routes = [
+  {
+    path:'',redirectTo:'customer',pathMatch:'full',
+
+  },
+  {
+    path:'user',component:UserComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
