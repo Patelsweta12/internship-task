2dea5e568b04c337fa426659715a30363ae0aa74
Patelsweta12 <swetapatel11114@gmail.com>
2023-06-27 13:55:06 +0530

    form row col folder addeddddd

--- bascis/formBootstrap/script.js
+++ bascis/formBootstrap/script.js
@@ -1,5 +1,27 @@
-let submitBtn = document.getElementById('submit')
-submitBtn.addEventListener("click",(e)=>{
+let submitBtn= document.getElementById("submit")
+submitBtn.addEventListener("click" , e =>{
     e.preventDefault();
+    let userNmae= document.getElementById("userNmae").value;
+    let alphNumericRegex = /^[A-Za-z]+$/;
+    if(userNmae.trim()==''){
+        document.getElementById("usernameError").innerHTML="User name is required"
+    }
+    else if(!userNmae.match(alphNumericRegex)){
+        document.getElementById("usernameError").innerHTML="Enter only characters"
+
+    }
+    else{
+        document.getElementById("usernameError").innerHTML=" "
+
+    }
+    let dob= document.getElementById("dob").value;
+    if(dob.trim()==''){
+        document.getElementById("dobError").innerHTML="dob name is required"
+    }
+
+    let phoneNo= document.getElementById("phoneNo").value;
+    if(dob.trim()==''){
+        document.getElementById("phError").innerHTML="Phone number is required"
+    }
 
 })
